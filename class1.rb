14574c2fd24294c03492b5c9c3dd32f11419af80
Mateo Olarte <mateo.olarte8@gmail.com>
2017-05-09 19:19:24 -0500

    fundamentals ruby

---
+++ class1.rb
@@ -0,0 +1,18 @@
+# puts 1, 'hola mundo'
+
+# # Recibe un valor asignado por el usuario
+# print "Cual es tu nombre? "
+# your_name = gets.chomp
+# print "Cual es tu edad? "
+# your_age = gets.chomp
+# puts "Hola #{your_name} tu edad es #{your_age}"
+
+num1 = 20
+num2 = 23
+num3 = 30
+num4 = 40
+
+resultado = num1 + num2 + num3 + num4
+promedio = resultado / 4
+
+puts "Tu promedio es #{promedio}"
