518e99760e9de3e389e2f577a014ffaf2167e5be
Mateo Olarte <mateo.olarte8@gmail.com>
2017-05-12 20:51:31 -0500

    methods & files ruby

---
+++ class4.rb
@@ -0,0 +1,31 @@
+# def fetch hash, key, default
+#   if hash[key] == nil
+#     default
+#   else
+#     hash[key]
+#   end
+# end
+# puts fetch({"id" => 01, "name" => "mateo", "program" => "makeit" }, "hola", "simon")
+
+# def average arreglo
+#   suma = 0
+#   arreglo.each do |element|
+#     suma = suma + element
+#   end
+#   resultado = suma / arreglo.length
+# end
+#
+# puts average [11,4,5,6,10]
+
+# def contat arreglo1, arreglo2
+#   arreglofinal = []
+#   arreglo1.each do |element|
+#     arreglofinal << element
+#   end
+#   arreglo2.each do |element2|
+#     arreglofinal << element2
+#   end
+#   arreglofinal
+# end
+#
+# puts contat [1,2,3], [4,5,6]
