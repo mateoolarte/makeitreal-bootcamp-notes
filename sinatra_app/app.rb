50031ff3ac9eece0151220efed3b70a73fb95a4b
Mateo Olarte <mateo.olarte8@gmail.com>
2017-05-23 23:05:38 -0500

    get - post redirect errors sinatra

--- sinatra_app/app.rb
+++ sinatra_app/app.rb
@@ -18,13 +18,43 @@ require "sinatra"
 #   puts "SORT BY=#{params[:sort]}"
 # end
 
-get "/customers" do
-  @customers = [
-    { nombre: "Mateo", edad: 20 },
-    { nombre: "David", edad: 20 },
-    { nombre: "Jaime", edad: 23 }
-  ]
-  @cliente = params[:nombre]
-  @nombre = @customers.find {|customer| customer[:nombre] == @cliente}
-  erb :customers
+# get "/customers" do
+#   @customers = [
+#     { nombre: "Mateo", edad: 20 },
+#     { nombre: "David", edad: 20 },
+#     { nombre: "Jaime", edad: 23 }
+#   ]
+#   @cliente = params[:nombre]
+#   @nombre = @customers.find {|customer| customer[:nombre] == @cliente}
+#   erb :customers
+# end
+
+get "/" do
+  erb :index
+end
+
+get "/login" do
+  erb :login
+end
+
+post "/login" do
+  email = params.fetch("email", "").strip
+  password = params.fetch("password", "").strip
+
+  if email.empty? || password.empty?
+    @errors = [{ message: "Email and password can't be empty" }]
+    return error(400, erb(:login))
+  end
+
+  unless email == "mateo@prueba.com" && password == "123456"
+    @errors = [{ message: "Email and password does not match" }]
+    return error(401, erb(:login))
+  end
+
+  redirect "user_account/#{params[:email]}"
+end
+
+get "/user_account/:email" do
+  @email = params[:email]
+  erb :account
 end
